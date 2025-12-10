350f55168b61d383690dd509b9a9cc0840072b62
kaangecer <153908306+kaangecer@users.noreply.github.com>
2025-12-10 17:17:00 +0000

    Update templates and add signup page

--- app.py
+++ app.py
@@ -6,6 +6,19 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")  # UI screen
 
+@app.route("/products")
+def products():
+    return render_template("products.html")  # TODO: create
+
+@app.route("/producers")
+def producers():
+    return render_template("producers.html")  # TODO: create
+
+@app.route("/join", methods=["GET", "POST"])
+def join():
+    # later you can handle form POST here
+    return render_template("join.html")
+
 @app.route("/login", methods=["GET", "POST"])
 def login():
     return render_template("login.html")  # UI screen
