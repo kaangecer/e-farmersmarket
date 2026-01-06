78598d03e1bfc1fa6cb4bb6be194e325907492ec
kaangecer <153908306+kaangecer@users.noreply.github.com>
2026-01-06 19:00:55 +0000

    extended forms to singup page.

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template
-
-from forms.forms import LoginForm
+from forms.forms import LoginForm, SignupForm
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "dev-secret-change-me"
@@ -11,26 +10,32 @@ def home():
 
 @app.route("/products")
 def products():
-    return render_template("products.html")  # TODO: create
+    return render_template("products.html")
 
 @app.route("/producers")
 def producers():
-    return render_template("producers.html")  # TODO: create
+    return render_template("producers.html") 
 
 #karte subpage
 @app.route("/maps")
 def maps():
-    return render_template("maps.html")  # TODO: create
+    return render_template("maps.html") 
 
 #warenkorb subpage
 @app.route("/cart")
 def cart():
-    return render_template("cart.html")  # TODO: create
+    return render_template("cart.html")
 
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
-    # later you can handle form POST here
-    return render_template("signup.html") # UI screen
+    form = SignupForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        username = form.username.data
+        email = form.email.data
+        password = form.password.data
+        pass
+    return render_template("signup.html", form=form)
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -39,7 +44,7 @@ def login():
         username = form.username.data
         password = form.password.data
         pass
-    return render_template("login.html", form=form)  # UI screen
+    return render_template("login.html", form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- forms/forms.py
+++ forms/forms.py
@@ -1,8 +1,16 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import InputRequired, Length
+from wtforms.validators import InputRequired, Email, Length
 
 class LoginForm(FlaskForm):
     username = StringField("Username", validators=[InputRequired(), Length(min=3)])
     password = PasswordField("Password", validators=[InputRequired(), Length(min=6)])
     submit = SubmitField("Login")
+
+class SignupForm(FlaskForm):
+    name = StringField("Name", validators=[InputRequired(), Length(min=2, max=50)])
+    username = StringField("Username", validators=[InputRequired(), Length(min=3, max=25)])
+    email = StringField("Email", validators=[InputRequired(), Email()])
+    password = PasswordField("Password", validators=[InputRequired(), Length(min=6)])
+    submit = SubmitField("Join now")
+
