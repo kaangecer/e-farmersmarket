1ebce65f03eb2a26606f912ef2150f0e7375e005
kaangecer <153908306+kaangecer@users.noreply.github.com>
2026-01-07 14:49:29 +0000

    SQLAlchemy configuration

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask, render_template
 from forms.forms import LoginForm, SignupForm, CartForm
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "dev-secret-change-me"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///farmersmarket.db"
+app.config("SQLALCHEMY_TRACK_MODIFICATIONS") = False
+db.init_app(app)
 
 @app.route("/")
 def home():
